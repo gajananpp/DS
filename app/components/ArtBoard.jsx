86734ffe2d093b8d6e6b2ffa30352972a7e71384
gajananpp <gajananpp@users.noreply.github.com>
2017-02-27 01:05:46 +0530

    Added position tooltip and image widget filters

--- app/components/ArtBoard.jsx
+++ app/components/ArtBoard.jsx
@@ -7,6 +7,8 @@ import { Scrollbars } from 'react-custom-scrollbars';
 import { stateToHTML } from 'draft-js-export-html';
 import DraggableDiv from './DraggableDiv';
 
+import $ from 'jquery';
+
 
 
 class ArtBoard extends React.Component {
@@ -24,17 +26,127 @@ class ArtBoard extends React.Component {
 		// console.log(document.getElementById('presentation-artboard').innerHTML.toString());
 	}
 
+	componentDidMount() {
+		// let artBoardPos = {};
+		// let offset = $("#sb-wrapper").offset();
+		// // console.log(offset);
+
+		// let canvasEl = document.getElementById("mouse-sniper");
+		// let ctx = canvasEl.getContext('2d');
+
+		// console.log(canvasEl);
+
+		// $("#sb-wrapper").on("mousemove", (event) => {
+		// 		console.log(event.pageX, event.pageY);
+		// 		ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
+				
+		// 		ctx.beginPath();
+		// 		ctx.moveTo(event.pageX - offset.left, 0);
+		// 		ctx.lineTo(event.pageX - offset.left, event.pageY - offset.top);
+		// 		ctx.stroke();
+				
+		// 		ctx.beginPath();
+		// 		ctx.moveTo(0, event.pageY - offset.top);
+		// 		ctx.lineTo(event.pageX - offset.left, event.pageY - offset.top);
+		// 		ctx.stroke();
+
+		// 		ctx.beginPath();
+		// 		ctx.moveTo(canvasEl.width, event.pageY - offset.top);
+		// 		ctx.lineTo(event.pageX - offset.left, event.pageY - offset.top);
+		// 		ctx.stroke();
+
+		// 		ctx.beginPath();
+		// 		ctx.moveTo(event.pageX - offset.left, canvasEl.height);
+		// 		ctx.lineTo(event.pageX - offset.left, event.pageY - offset.top);
+		// 		ctx.stroke();
+
+		// });
+		// 
+		// let tooltipEl = $("#position-tooltip")[0];
+		
+		// let tooltipPositions = {
+		// 	top: tooltipEl.style.top,
+		// 	left: tooltipEl.style.left,
+		// 	bottom: tooltipEl.style.bottom,
+		// 	right: tooltipEl.style.right 
+		// }; 
+		
+		 
+		let container = $("#sb-wrapper > div")[0]
+		console.log(container);
+
+		let canvasEl = $("#mouse-sniper")[0];
+		let ctx = canvasEl.getContext('2d');
+		
+		container.addEventListener("mousemove", (e) => {
+			let rect = container.getBoundingClientRect();
+
+			let x = e.clientX + container.scrollLeft - rect.left;
+			let y = e.clientY + container.scrollTop - rect.top;
+
+			ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
+
+			if (y > 700) {
+				$("#position-tooltip")[0].style.top = `${y-100}px`;
+				$("#position-tooltip")[0].style.left = `${x+15}px`;
+			} else {
+				$("#position-tooltip")[0].style.top = `${y+15}px`;
+				$("#position-tooltip")[0].style.left = `${x+15}px`;
+			}
+
+
+			$("#top-pos").text(`Top: ${y} px`);
+			$("#left-pos").text(`Left: ${x} px`);
+			$("#bottom-pos").text(`Bottom: ${canvasEl.height - y} px`);
+			$("#right-pos").text(`Right: ${canvasEl.width - x} px`);
+				
+			ctx.beginPath();
+			ctx.moveTo(x, 0);
+			ctx.lineTo(x, y);
+			ctx.stroke();
+				
+			ctx.beginPath();
+			ctx.moveTo(0, y);
+			ctx.lineTo(x, y);
+			ctx.stroke();
+
+			ctx.beginPath();
+			ctx.moveTo(canvasEl.width, y);
+			ctx.lineTo(x, y);
+			ctx.stroke();
+
+			ctx.beginPath();
+			ctx.moveTo(x, canvasEl.height);
+			ctx.lineTo(x, y);
+			ctx.stroke();
+
+
+
+			console.log(x, y);
+		});
+		
+		
+	}
+
 	render() {
 				// console.log(document.getElementById('presentation-artboard').innerHTML.toString());
 
 		return (
-			<Scrollbars>
-				<div style={{width: this.props.width, height: this.props.height, background: this.props.backgroundColor + ` url(${this.props.backgroundImgURL})` }}>
+			<Scrollbars id="sb-wrapper">
+			<div id="artboard-wrapper">
+				<canvas id="mouse-sniper" width={this.props.width} height={this.props.height} style={{position: "absolute", top: "0", left: "0", zIndex: 1000, opacity: 0.5, pointerEvents: 'none'}}></canvas>
+				<span id="position-tooltip" style={{position: "absolute", top: "0", left: "0", zIndex: 1000, opacity: 0.5, pointerEvents: 'none', backgroundColor: 'rgba(0, 0, 0, 0.8)', color: 'white', padding: 10}}>
+					<span id="top-pos"></span><br />
+					<span id="left-pos"></span><br />
+					<span id="bottom-pos"></span><br />
+					<span id="right-pos"></span>
+				</span>
+				<div id="artboard-presentation" style={{width: this.props.width, height: this.props.height, background: this.props.backgroundColor + ` url(${this.props.backgroundImgURL})`, position: 'absolute', top: 0, left: 0 }}>
 					{ this.props.widgetsUsed.map((obj, index) => (
-						<DraggableDiv zIndex={obj.widgetIndex*100} key={index}>{obj.element}</DraggableDiv>
+						<DraggableDiv zIndex={obj.widgetIndex} key={index}>{obj.element}</DraggableDiv>
 					)) }
 				</div>
-
+			</div>
 			</Scrollbars>
 		);
 	}
--- app/widgets/ImageWidget.jsx
+++ app/widgets/ImageWidget.jsx
@@ -3,12 +3,17 @@ import React from 'react';
 import Dialog from 'material-ui/Dialog';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+import CheckBox from 'material-ui/CheckBox';
 
 class ImageWidget extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			imgSrc: '',
+			
+			grayImage: false,
+			sepiaImage: false,
+
 			dialogOpen: true,
 		};
 	}
@@ -19,6 +24,30 @@ class ImageWidget extends React.Component {
 		this.setState({
 			imgSrc : text,
 		});
+	}
+
+	handleGrayFilter = (event, isInputChecked) => {
+		if (isInputChecked) {
+			this.imgEl.style.filter = "grayscale(100%)";
+		} else {
+			this.imgEl.style.filter = "";
+		}
+		this.setState({
+			grayImage: isInputChecked
+		});
+		console.log(isInputChecked);
+	}
+
+	handleSepiaFilter = (event, isInputChecked) => {
+		if (isInputChecked) {
+			this.imgEl.style.filter = "sepia(100%)";
+		} else {
+			this.imgEl.style.filter = "";
+		}
+		this.setState({
+			sepiaImage: isInputChecked
+		});
+		console.log(isInputChecked);
 	}
 
 	handleOpen = () => {
@@ -33,12 +62,15 @@ class ImageWidget extends React.Component {
 		});
 	}
 
+	componentDidMount() {
+	}
+
 
 	render() {
 		return (
 			<div style={{width: '100%', height: '100%'}} onDoubleClick={ this.handleOpen } >
 				<div style={{width: '100%', height: '100%'}}>
-					<img style={{width: '100%', height: '100%'}} src={this.state.imgSrc} alt={this.state.imgSrc} />
+					<img ref={imgEl => this.imgEl = imgEl} className="image-widget" style={{width: '100%', height: '100%'}} src={this.state.imgSrc} alt={this.state.imgSrc} />
 				</div>
 
 				<Dialog
@@ -52,6 +84,19 @@ class ImageWidget extends React.Component {
 							<span style={{marginRight: 20}}>Image Link :</span>
 							<TextField hintText="Image URL" defaultValue={this.state.imgSrc} onChange={ this.handleUrlChange } />
 						</label>
+						<br />
+						<div>
+							<CheckBox
+								label="Black And White Filter"
+								checked={ this.state.grayImage } 
+								onCheck={ this.handleGrayFilter }
+							/>
+							<CheckBox
+								label="Sepia Filter"
+								checked={ this.state.sepiaImage } 
+								onCheck={ this.handleSepiaFilter }
+							/>
+						</div>
 					</div>
 				</Dialog>
 			</div>
